ff7cf6643b824032a23e7a9076aac20a1296fcde
Ethan Buck <egbuck96@gmail.com>
2020-07-30 14:28:22 -0400

    Fixes made and tests passing - Had to change var names in ResourceAllocation

--- ormm/opt/mathprog/problems/resource_allocation.py
+++ ormm/opt/mathprog/problems/resource_allocation.py
@@ -28,30 +28,30 @@ class ResourceAllocation(pyo.AbstractModel):
     def create_abstract_model(self):
         """Create the abstract model for Resource Allocation Problem
         """
-        self.Products = pyo.Set()
-        self.Machines = pyo.Set()
-        self.Profits = pyo.Param(self.Products)
-        self.ProcessTimes = pyo.Param(self.Machines, self.Products)
-        self.MaxTimes = pyo.Param(self.Machines)
-        self.MaxDemand = pyo.Param(self.Products)
-        self.Produce = pyo.Var(self.Products, within = pyo.NonNegativeIntegers, bounds = self._get_bounds)
+        self.Activities = pyo.Set()
+        self.Resources = pyo.Set()
+        self.Values = pyo.Param(self.Activities)
+        self.ResourceNeeds = pyo.Param(self.Resources, self.Activities)
+        self.MaxResource = pyo.Param(self.Resources)
+        self.MaxActivity = pyo.Param(self.Activities)
+        self.NumActivity = pyo.Var(self.Activities, within = pyo.NonNegativeIntegers, bounds = self._get_bounds)
 
         self.OBJ = pyo.Objective(rule = self._obj_expression, sense = pyo.maximize)
-        self.ResourceConstraint = pyo.Constraint(self.Machines, rule = self._resource_constraint_rule)
+        self.ResourceConstraint = pyo.Constraint(self.Resources, rule = self._resource_constraint_rule)
 
     def _get_bounds(self, model, p):
         """Upper Bounds for the Decision Vars based on Maximum Demand
         """
-        return (0, self.MaxDemand[p])
+        return (0, self.MaxActivity[p])
 
     def _obj_expression(self, model):
         """Objective Expression: Maximizing Profit
         """
-        return pyo.summation(self.Profits, self.Produce)
+        return pyo.summation(self.Values, self.NumActivity)
 
     def _resource_constraint_rule(self, model, m):
         """Constraints for Scarce Resources"""
-        return sum(self.ProcessTimes[m, p] * self.Produce[p] for p in self.Products) <= self.MaxTimes[m]
+        return sum(self.ResourceNeeds[m, p] * self.NumActivity[p] for p in self.Activities) <= self.MaxResource[m]
 
 def print_sol(instance):
     """Print the solution to the solved `instance`
