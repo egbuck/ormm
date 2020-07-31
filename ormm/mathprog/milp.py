d937302d74fde6dfce3b92ff234c18d4510b62b1
Ethan Buck <egbuck96@gmail.com>
2020-07-31 14:10:10 -0400

    Updated Docstrings in MILP module - more akin to numpydoc guidelines - still need to install this / set it up, but only once get other issues fixed - example: readthedocs.org not pulling in base class correctly

--- ormm/mathprog/milp.py
+++ ormm/mathprog/milp.py
@@ -1,33 +1,45 @@
+"""
+Module contains classes & functions for Mixed Integer Linear Programs (MILP).
+"""
 import pyomo.environ as pyo
 
 class ResourceAllocation(pyo.AbstractModel):
-    """Pyomo Abstract Model for the Resource Allocation Problem
+    """
+    Subclass of Pyomo Abstract Model for the Resource Allocation Problem.
 
-    :return: A pyomo abstract model with definitions for the Resource Allocation Problem
+    Parameters
+    ----------
+    *args, **kwargs : optional
+        Passed into Pyomo Abstract Model's `create_instance` 
+        to return Pyomo Concrete Model instead.
 
-    See Also
-    --------
-    Formulation: Documentation with definitions & details of the abstract model.
+    Notes
+    -----
+    The Resource Allocation Problem optimizes using scarce resources for valued activities.
+    .. math:: 
+       Max  \sum_{a \in A} V_aX_a\\
+       s.t. \sum_{a \in A} N_{r,a}X_a <= M_r \forall r \in R\\
+            0 <= X_a <= M_a \forall a \in A
 
     Examples
     --------
     Creating abstract model, an instance from data params, & solving instance.
 
+    >>> import pyomo.environ as pyo
     >>> model = ResourceAllocation()
-    >>> instance = model.create_instance(my_params.dat) # AMPL data file
+    >>> instance = model.create_instance("my_params.dat") # AMPL data file
     >>> opt = pyo.SolverFactory("glpk")
-    >>> opt.solve(instance)
+    >>> results = opt.solve(instance)
     """
-
-    def __init__(self):
-        """Constructor method - calls _create_abstract_model
-        """
+    def __init__(self, *args, **kwargs):
+        """Constructor method - calls _create_abstract_model"""
         super().__init__()
         self._create_abstract_model()
+        if args or kwargs:
+            self._return_concrete_model(*args, **kwargs)
 
     def _create_abstract_model(self):
-        """Create the abstract model for Resource Allocation Problem
-        """
+        """Create the abstract model for Resource Allocation Problem."""
         self.Activities = pyo.Set()
         self.Resources = pyo.Set()
         self.Values = pyo.Param(self.Activities)
@@ -39,14 +51,16 @@ class ResourceAllocation(pyo.AbstractModel):
         self.OBJ = pyo.Objective(rule = self._obj_expression, sense = pyo.maximize)
         self.ResourceConstraint = pyo.Constraint(self.Resources, rule = self._resource_constraint_rule)
 
+    def _return_concrete_model(self, *args, **kwargs):
+        """Return Pyomo Concrete Model, given data."""
+        return self.create_instance(*args, **kwargs)
+
     def _get_bounds(self, model, p):
-        """Upper Bounds for the Decision Vars based on Maximum Demand
-        """
+        """Upper Bounds for the Decision Vars based on Maximum Demand."""
         return (0, self.MaxActivity[p])
 
     def _obj_expression(self, model):
-        """Objective Expression: Maximizing Profit
-        """
+        """Objective Expression: Maximizing Value"""
         return pyo.summation(self.Values, self.NumActivity)
 
     def _resource_constraint_rule(self, model, m):
@@ -54,8 +68,12 @@ class ResourceAllocation(pyo.AbstractModel):
         return sum(self.ResourceNeeds[m, p] * self.NumActivity[p] for p in self.Activities) <= self.MaxResource[m]
 
 def print_sol(instance):
-    """Print the solution to the solved `instance`
-    :param instance: A solved pyomo.environ.ConcreteModel
+    """Print the solution to the solved `instance`.
+
+    Parameters
+    ----------
+    instance : pyomo.core.base.PyomoModel.AbstractModel
+        A solved model to retrieve objective & variable values.
     """
     print(f"Objective Value: ${instance.OBJ():,}")
     for v in instance.component_objects(pyo.Var, active=True):
