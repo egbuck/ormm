ce92c21abfef6fff54aa9ed8fb505503ff70433f
Ethan Buck <egbuck96@gmail.com>
2020-08-04 17:18:13 -0400

    Fixed tests for rental scheduling problem - now functional!

--- tests/test_scheduling.py
+++ tests/test_scheduling.py
@@ -23,28 +23,25 @@ def test_employee_simple():
 def test_rental_simple():
     model = scheduling(prob_class="rental")
     instance1 = model.create_instance(RENTAL_DATA)
-    instance1.pprint()
     instance2 = scheduling(prob_class="rental", filename=RENTAL_DATA)
-    # for inst in [instance1, instance2]:
-    #     instance, results = solve_instance(inst)
-    #     assert instance.OBJ() == 295
-    #     for v in instance.component_objects(pyo.Var, active=True):
-    #         print({index: v[index].value for index in v})
-    #         assert {index: v[index].value for index in v} == {
-    #             ("Sat", "DailyWeekend"): 0.0,
-    #             ("Sun", "DailyWeekend"): 0.0,
-    #             ("Mon", "DailyWeekDay"): 5.0,
-    #             ("Tue", "DailyWeekDay"): 0.0,
-    #             ("Wed", "DailyWeekDay"): 9.0,
-    #             ("Thu", "DailyWeekDay"): 0.0,
-    #             ("Fri", "DailyWeekDay"): 4.0,
-    #             ("Mon", "ThreeWeekDay"): 0.0,
-    #             ("Tue", "ThreeWeekDay"): 0.0,
-    #             ("Wed", "ThreeWeekDay"): 2.0,
-    #             ("Sat", "Weekend"): 0.0,
-    #             ("Mon", "AllWeekDay"): 0.0,
-    #             ("Sat", "AllWeek"): 5.0
-    #         }
+    for inst in [instance1, instance2]:
+        instance, results = solve_instance(inst)
+        assert instance.OBJ() == 1830.0
+        for v in instance.component_objects(pyo.Var, active=True):
+            assert {index: v[index].value for index in v} == {
+                ('Mon', 'DailyWeekDay'): 1.0,
+                ('Tue', 'DailyWeekDay'): 0.0,
+                ('Wed', 'DailyWeekDay'): 3.0,
+                ('Thu', 'DailyWeekDay'): 0.0,
+                ('Fri', 'DailyWeekDay'): 2.0,
+                ('Sat', 'DailyWeekend'): 0.0,
+                ('Sun', 'DailyWeekend'): 0.0,
+                ('Mon', 'ThreeWeekDay'): 1.0,
+                ('Tue', 'ThreeWeekDay'): 0.0,
+                ('Wed', 'ThreeWeekDay'): 0.0,
+                ('Sat', 'Weekend'): 0.0,
+                ('Mon', 'AllWeekDay'): 4.0,
+                ('Sat', 'AllWeek'): 4.0}
 
 
 if __name__ == "__main__":
