e6c15947408bfff7e8ed224d096de7cf3fb5972c
Ethan Buck <egbuck96@gmail.com>
2020-08-04 14:14:27 -0400

    Added data file & parts of model for rental method - tests added, but won't pass - need to fix constraints - tests also need updated values for obj & vars - data file added - commented sections at bottom   - can probably be removed now

--- ormm/mathprog/scheduling.py
+++ ormm/mathprog/scheduling.py
@@ -41,7 +41,53 @@ def _rental(**kwargs):
         if any given, returns pyomo concrete model instead, with these passed
         into pyomo's `create_instance`.
     """
-    pass
+    def _obj_expression(model):
+        """Objective Expression: Minimizing Number of Workers"""
+        my_expr = 0
+        for (period, plan) in model.PlanToPeriod:
+            my_expr += model.PlanCosts[plan] * model.NumRent[(period, plan)]
+        return my_expr
+
+    def _period_reqs_constraint_rule(model, p):
+        """Constraints for having enough workers per period"""
+        first_member = model.Periods.first()
+        last_member = model.Periods.last()
+        next_member = model.Periods.next(p, step=1)
+        index = model.Periods.ord(p)  # 1 based!!
+        set_member = model.Periods[index]  # ?
+        print((first_member, last_member, next_member, index, set_member))
+        if p == min(model.Periods):
+            return (model.PeriodReqs[p],
+                    model.NumWorkers[p] + model.NumWorkers[max(model.Periods)],
+                    None)
+        else:
+            return (model.PeriodReqs[p],
+                    model.NumWorkers[p] + model.NumWorkers[p - 1], None)
+
+    # Create the abstract model & dual suffix
+    model = pyo.AbstractModel()
+    model.dual = pyo.Suffix(direction=pyo.Suffix.IMPORT)
+    # Define sets/params that are always used
+    model.Periods = pyo.Set(ordered=True)
+    model.Plans = pyo.Set()
+    model.PlanToPeriod = pyo.Set(dimen=2)
+    model.PeriodReqs = pyo.Param(model.Periods)
+    model.PlanCosts = pyo.Param(model.Plans)
+    model.PlanLengths = pyo.Param(model.Plans)
+    # Define decision variables
+    model.NumRent = pyo.Var(
+        model.PlanToPeriod,
+        within=pyo.NonNegativeIntegers)
+    # Define objective & constraints
+    model.OBJ = pyo.Objective(rule=_obj_expression, sense=pyo.minimize)
+    model.PeriodReqsConstraint = pyo.Constraint(
+        model.Periods,
+        rule=_period_reqs_constraint_rule)
+    # Check if returning concrete or abstract model
+    if kwargs:
+        return model.create_instance(**kwargs)
+    else:
+        return model
 
 
 def _employee(**kwargs):
--- tests/test_scheduling.py
+++ tests/test_scheduling.py
@@ -3,11 +3,34 @@ import pyomo.environ as pyo
 from ormm.mathprog import scheduling
 from tests.methods import solve_instance
 
+DATA_PATH = "ormm/mathprog/example_data/"
+EMPLOYEE_DATA = DATA_PATH + "employee.dat"
+RENTAL_DATA = DATA_PATH + "rental.dat"
+
 
 def test_employee_simple():
-    instance = scheduling(filename="ormm/mathprog/example_data/employee.dat")
-    instance, results = solve_instance(instance)
-    assert instance.OBJ() == 295
-    for v in instance.component_objects(pyo.Var, active=True):
-        assert {index: v[index].value
-                for index in v} == {1: 55, 2: 115, 3: 85, 4: 0, 5: 25, 6: 15}
+    model = scheduling()
+    instance1 = model.create_instance(EMPLOYEE_DATA)
+    instance2 = scheduling(filename=EMPLOYEE_DATA)
+    for inst in [instance1, instance2]:
+        instance, results = solve_instance(inst)
+        assert instance.OBJ() == 295
+        for v in instance.component_objects(pyo.Var, active=True):
+            assert {index: v[index].value for index in v} == {
+                1: 55, 2: 115, 3: 85, 4: 0, 5: 25, 6: 15}
+
+
+def test_rental_simple():
+    model = scheduling(prob_class="rental")
+    instance1 = model.create_instance(RENTAL_DATA)
+    instance2 = scheduling(prob_class="rental", filename=RENTAL_DATA)
+    for inst in [instance1, instance2]:
+        instance, results = solve_instance(inst)
+        assert instance.OBJ() == 295
+        for v in instance.component_objects(pyo.Var, active=True):
+            assert {index: v[index].value for index in v} == {
+                1: 55, 2: 115, 3: 85, 4: 0, 5: 25, 6: 15}
+
+
+if __name__ == "__main__":
+    test_rental_simple()
