15430f2c5beb8ca7ee6e283b13bc1961ad69187c
Ethan Buck <egbuck96@gmail.com>
2020-08-10 12:57:28 -0400

    Added to docstring in transportation

--- ormm/mathprog/network_flow.py
+++ ormm/mathprog/network_flow.py
@@ -3,32 +3,40 @@ import pyomo.environ as pyo
 
 def transportation(**kwargs):
     """
-    Factory method for the transportation problem class
+    Factory method for the transportation problem class.
 
-    This is a bipartite network with m supply nodes, n destination nodes
-    If not possible to ship from i to j, a large cost M should be passed
+    This is a bipartite network with m supply nodes and n destination nodes.
+    If not possible to ship from i to j, a large cost M should be passed.
 
     Assumes the feasibility property holds
-        (total supply equals total demand)
-        then becomes balanced TP
-    Will be modified so req satisfied
-        let \\delta be the excess
-        add dummy source with index m + 1 if demand > supply
-            s_{m+1} = \\delta ; c_{m+1,j}=0 for all j
-        add dummy demand with index n + 1 if supply > demand
-            d_{n+1} = \\delta ; c_{i,n+1}=0 for all i
-    d.v.: x_{i,j} - flow from source i to destination j
+        (total supply equals total demand) -
+        then becomes balanced TP.
+    You can modify data so this requirement is satisfied.
+        Let :math:`\\delta` be the excess amount (positive).
+        Add  a dummy source to the data with index m + 1 if demand > supply.
+            :math:`s_{m+1} = \\delta\\text{ ; }c_{m+1,j}=0 \\forall j`
+        Add a dummy demand to the data with index n + 1 if supply > demand.
+            :math:`d_{n+1} = \\delta\\text{ ; }c_{i,n+1}=0 \\forall i`
+
+    Notes
+    -----
+    .. math::
+        \\text{Min} \\sum_{i \\in I}\\sum_{j \\in J}C_{i,j}X_{i,j}
+        \\text{s.t. } \\sum_{j \\in J} X_{i,j} = S_i \\quad \\forall i \\in I
+        \\sum_{i \\in I} X_{i,j} = D_j \\quad \\forall j \\in J
+        X_{i,j} \\geq 0\\text{, int} \\quad \\forall i \\in I\\text{, }j \\in J
     """
     def _obj_expression(model):
-        """Objective Expression: Maximizing Value"""
+        """Objective Expression: Minimizing Shipping Costs"""
         return pyo.summation(model.ShippingCosts, model.Flows)
 
     def _supply_constraint_rule(model, i):
-        """Constraints for Scarce Resources"""
+        """Constraints for flows from supply"""
         return sum(model.Flows[i, j]
                    for j in model.Destinations) == model.Supply[i]
 
     def _demand_constraint_rule(model, j):
+        """Constraints for flows from demands"""
         return sum(model.Flows[i, j]
                    for i in model.Supply) == model.Demand[j]
 
