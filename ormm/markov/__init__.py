217413a87a9230e20864b3569fa2fb1662ad8e89
Ethan Buck <egbuck96@gmail.com>
2020-08-12 19:10:36 -0400

    Working through examples now - Think this will help shed light on what I actually need to implement  - Maybe it's just examples  - Maybe it's using a distribution somehow    - thought about it, this is hard to generalize    - may just end up showing examples of how to do this - Markov class may just be showing many applications   - a teaching tool   - making abstract concepts into practical real life situations

--- ormm/markov/__init__.py
+++
@@ -1,3 +0,0 @@
-from ormm.markov.main import markov_discrete_time
-
-__all__ = ["markov_discrete_time"]
--- tests/test_markov.py
+++ tests/test_markov.py
@@ -1,13 +1,16 @@
-import json
-
-from ormm.markov import markov_discrete_time
-
-MARKOV_PATH = "ormm/markov/example_data/"
-INCOME_TAX_DATA = MARKOV_PATH + "income_tax.json"
+from quantecon.markov import MarkovChain
 
 
-def _later_test_for_markov():
-    json_data = json.load(INCOME_TAX_DATA)
-    audit_history = json_data["History"]
-    years = json_data["Years"]
-    markov_discrete_time(data=audit_history, time_data=years)
+def income_tax_audit():
+    """A simple example of markov chain"""
+    # Define MarkovChain object - P is transition matrix
+    income_tax_audit = MarkovChain(
+        P=[[0.6, 0.4],
+           [0.5, 0.5]], state_values=[0, 1])
+    # Show useful attributes/methods
+    print("cdfs()")
+    print(income_tax_audit.cdfs)
+    print("Simulate")
+    print(income_tax_audit.simulate(ts_length=25))
+    print("Stationary Distributions")
+    print(income_tax_audit.stationary_distributions)
