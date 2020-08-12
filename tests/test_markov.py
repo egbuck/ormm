f7e731b03547d7d863d67369198b943520ec1566
Ethan Buck <egbuck96@gmail.com>
2020-08-12 19:58:39 -0400

    Students at university example - Shows some things that could be useful for markov chain analysis - Thinking will implement my own class of this   - Don't need as much   - Just cdf, simulate, and stationary_distributions   - Then not depending on other library   - also, can add my own methods/attributes for...     - calculating average rate of an event/activity (arrival/service     rate)     - calculating average interarrival time (1 / average rate)     - maybe distribution analysis of steady states (not sure...)

--- tests/test_markov.py
+++ tests/test_markov.py
@@ -14,3 +14,21 @@ def income_tax_audit():
     print(income_tax_audit.simulate(ts_length=25))
     print("Stationary Distributions")
     print(income_tax_audit.stationary_distributions)
+
+
+def students_at_university():
+    """A simple example of markov chain"""
+    # Define MarkovChain object - P is transition matrix
+    # Data - this would be more like data from a simulation
+    #    or perhaps for later, the steady state probabilities
+    data = {0: 10, 1: 20, 2: 30, 3: 15, 4: 15, 5: 10}
+    # Average arrival rate
+    arrival_rate = sum(hour * count for hour, count in data.items())
+    print(arrival_rate)
+    # Average interarrival time in minutes
+    interarrival_time = (1 / arrival_rate)
+    print(interarrival_time * 60)
+
+
+if __name__ == "__main__":
+    students_at_university()
